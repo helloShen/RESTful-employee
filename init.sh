3fa1180152fb6d57b4c73d678ad5529e3df9427a
helloShen <symantec__@hotmail.com>
2022-06-03 18:16:41 -0400

    migrate from mern-employee

---
+++ init.sh
@@ -0,0 +1,21 @@
+!/bin/zsh
+
+### git
+git init
+touch .gitignore
+ignoreScript='# vscode\n.vscode/\n# npm\npackage-lock.json\nnode_modules/'
+echo $ignoreScript > .gitignore
+
+### project
+# mkdir dist
+# mkdir src
+# touch index.html
+
+### npm
+#npm init -y
+
+### server
+npm install mongodb mongoose express cors dotenv helmet morgan jade express-handlebars
+npm install --save-dev nodemon
+
+
